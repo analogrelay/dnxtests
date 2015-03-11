43f165e155ef09c8bfb1aa26a62c41997ed9e692
anurse <andrew@andrewnurse.net>
2015-03-11 15:16:16 -0700

    add some tests and set the sdk version to the dev version

---
+++ src/ALittleBitOfJson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ALittleBitOfJson
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Here's some JSON:");
+            Console.WriteLine(new JObject(
+                new JProperty("it's", "json")).ToString());
+            Console.ReadLine();
+        }
+    }
+}
